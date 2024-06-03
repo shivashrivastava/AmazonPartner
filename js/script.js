d89c9c66619b1fdfa2a9744222a8445ab74732f9
shivashrivastava <66993256+shivashrivastava@users.noreply.github.com>
2024-06-03 23:36:49 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,5 @@ Your Email : document.getElementById("Your Email").Value,
 Message : document.getElementById("Message").Value,
 }
 emailjs.send("service_kl8fakc","template_mxh4ijg",parms).then(alert("Email Sent!!"))
+  
+}
