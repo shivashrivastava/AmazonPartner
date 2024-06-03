3c6fd205d03a161ce95d16c4dc5df142b219e05b
shivashrivastava <66993256+shivashrivastava@users.noreply.github.com>
2024-06-03 23:29:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function sendMail(){
 let parms = {
+Your Email : document.getElementById("Your Email").Value,
+Message : document.getElementById("Message").Value,
 }
-Message
+emailjs.send("service_kl8fakc","template_mxh4ijg",parms).then(alert("Email Sent!!"))
